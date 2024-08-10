72821691661f13dec628153b11c3eaff9a579d42
Vinicius Serpa <vinicius-serpa@live.com>
2024-08-10 20:57:11 -0300

    add chi cors configuration

--- backend/internal/api/api.go
+++ backend/internal/api/api.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/vserpa/ask-me-anything-app/internal/store/pgstore"
 )
 
@@ -24,6 +25,15 @@ func NewHandler(q *pgstore.Queries) http.Handler {
 
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID, middleware.Recoverer, middleware.Logger)
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 
 	r.Get("/subscribe/{room_id}", a.handleSubscribe)
 
